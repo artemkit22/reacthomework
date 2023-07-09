11165764c527b49c381d3095368c086434ec2088
artemkit22 <artem1357984@gmail.com>
2023-07-09 20:11:22 +0300

    HWreact2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-function App() {
-    return (
-        <div>homework1</div>
-    );
-}
+import {useState, useEffect} from "react";
+import Post from "./components/Post";
 
-export default App;
+// const App = () => {
+//     const [post, setPost] = useState([])
+//     useEffect(()=>{
+//         fetch ('https://jsonplaceholder.typicode.com/posts')
+//             .then(value => value.json())
+//             .then(value => setPost(value))
+//
+//     },[])
+
+
+//     return (<>
+//         {post.map(value => (<Post   post = {value} key ={value.id}/>))}
+//
+//     </>)
+//
+//     const [post, setPost] = useState([])
+//     useEffect(()=>{
+//         fetch ('https://api.spacexdata.com/v3/launches/')
+//             .then(value => value.json())
+//             .then(value => setPost(value))
+//
+//     },[])
+//
+//     return (<>
+//                  {post.filter(value => value.launch_year !== "2020").map(value => (<Post   post = {value} key ={value.mission_name}/>))}
+//
+//              </>)
+// }
+// export default App
---
+++ src/components/posts.js
@@ -0,0 +1,25 @@
+// import {useState} from "react";
+//
+// const Post = (props) => {
+//     const [status, setStatus] = useState(false)
+//
+//     return (<div>
+//         <h2>{props.post.launch_year}</h2>
+//         <h2>{props.post.mission_name}</h2>
+//         <h2>{props.post.links.mission_patch_small}</h2>
+//
+//     </div>)
+// }
+// export default Post// import {useState} from "react";
+// //
+// // const Post = (props) => {
+// //     const [status, setStatus] = useState(false)
+// //
+// //     return (<div>
+// //         <h2>{props.post.launch_year}</h2>
+// //         <h2>{props.post.mission_name}</h2>
+// //         <h2>{props.post.links.mission_patch_small}</h2>
+// //
+// //     </div>)
+// // }
+// // export default Post
